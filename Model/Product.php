718aa17991209a6148c97d2856031a87ab20856a
Josh Di Fabio <joshdifabio@gmail.com>
2016-11-18 11:57:44 +0000

    Add product model implementing our data interface

---
+++ Model/Product.php
@@ -0,0 +1,9 @@
+<?php
+namespace SnowIO\AttributeOptionCode\Model;
+
+use SnowIO\AttributeOptionCode\Api\Data\ProductInterface;
+
+class Product extends \Magento\Catalog\Model\Product implements ProductInterface
+{
+
+}
